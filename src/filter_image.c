8c2247446841709b2116ac6d6fbccd4010d2ee8a
Keith Chester <kchester@gmail.com>
2020-04-09 22:43:27 -0700

    feature: highpass, sharpen, and emboss filters finished

--- src/filter_image.c
+++ src/filter_image.c
@@ -63,10 +63,10 @@ image convolve_image(image im, image filter, int preserve)
 
                     for(int fy = 0; fy < filter.h; fy++){
                         for(int fx = 0; fx < filter.w; fx++){
-                            fValue = get_pixel(filter, fx, fx, 0);
+                            fValue = get_pixel(filter, fx, fy, 0);
                             
-                            nx = (-1 * fOffset ) + fx;
-                            ny = (-1 * fOffset ) + fy;
+                            nx = x + (-1 * fOffset ) + fx;
+                            ny = y + (-1 * fOffset ) + fy;
 
                             // //Zero padding = results in a 0 to
                             // //this piece of the convolution
@@ -78,7 +78,6 @@ image convolve_image(image im, image filter, int preserve)
 
                             imValue = get_pixel(im, nx, ny, c);
                             sum += fValue * imValue;
-                            // printf("(%d, %d) C (%d, %d)=> (%d,%d) :: %f * %f = %f\n", x, y, fx, fy, nx, ny, fValue, imValue, sum);
                         }
                     }
                     set_pixel(convolved, x, y, c, sum);
@@ -91,31 +90,23 @@ image convolve_image(image im, image filter, int preserve)
 
         for(int y = 0; y < im.h; y++){
             for(int x = 0; x < im.w; x++){
-                float sum = 0;
+                sum = 0;
                 
                 for(int fy = 0; fy < filter.h; fy++){
                     for(int fx = 0; fx < filter.w; fx++){
-                        float fValue = get_pixel(filter, fx, fx, 1);
-                        
-                        int nx = (-1 * fOffset ) + fx;
-                        int ny = (-1 * fOffset ) + fy;
+                        fValue = get_pixel(filter, fx, fy, 0);
 
-                        // //Zero padding = results in a 0 to
-                        // //this piece of the convolution
-                        // if(x < 0 || y < 0){
-                        //     continue;
-                        // } else if(x >= im.w || y >= im.h) {
-                        //     continue;
-                        // }
+                        nx = x + (-1 * fOffset) + fx;
+                        ny = y + (-1 * fOffset) + fy;
 
                         for(int c = 0; c < im.c; c++){
-                            float imValue = get_pixel(im, nx, ny, c);
+                            imValue = get_pixel(im, nx, ny, c);
+                            // printf("(%d, %d, %d) C (%d, %d, %d)=> (%d,%d) :: %f * %f = %f\n", x, y, c, fx, fy, 0, nx, ny, fValue, imValue, sum);
                             sum += fValue * imValue;
                         }
                     }
                 }
-
-                set_pixel(convolved, x, y, 1, sum);
+                set_pixel(convolved, x, y, 0, sum);
             }
         }
     }
@@ -125,20 +116,59 @@ image convolve_image(image im, image filter, int preserve)
 
 image make_highpass_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    image filter = make_image(3, 3, 1);
+    
+    filter.data[0] = 0;
+    filter.data[1] = -1;
+    filter.data[2] = 0;
+
+    filter.data[3] = -1;
+    filter.data[4] = 4;
+    filter.data[5] = -1;
+
+    filter.data[6] = 0;
+    filter.data[7] = -1;
+    filter.data[8] = 0;
+
+    return filter;
 }
 
 image make_sharpen_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    image filter = make_image(3, 3, 1);
+
+    filter.data[0] = 0;
+    filter.data[1] = -1;
+    filter.data[2] = 0;
+
+    filter.data[3] = -1;
+    filter.data[4] = 5;
+    filter.data[5] = -1;
+
+    filter.data[6] = 0;
+    filter.data[7] = -1;
+    filter.data[8] = 0;
+
+    return filter;
 }
 
 image make_emboss_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    image filter = make_image(3, 3, 1);
+
+    filter.data[0] = -2;
+    filter.data[1] = -1;
+    filter.data[2] = 0;
+
+    filter.data[3] = -1;
+    filter.data[4] = 1;
+    filter.data[5] = 1;
+
+    filter.data[6] = 0;
+    filter.data[7] = 1;
+    filter.data[8] = 2;
+
+    return filter;
 }
 
 // Question 2.2.1: Which of these filters should we use preserve when we run our convolution and which ones should we not? Why?
